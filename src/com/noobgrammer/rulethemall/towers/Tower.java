644a4903b271f135bdd550241850c59eaac78b48
bytehala <lemuel.dulfo@gmail.com>
2013-02-08 21:50:58 +0800

    solved - towers clickable. but not yet implemented.

--- src/com/noobgrammer/rulethemall/towers/Tower.java
+++ src/com/noobgrammer/rulethemall/towers/Tower.java
@@ -5,11 +5,12 @@ import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.extension.physics.box2d.util.constants.PhysicsConstants;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class Tower extends AnimatedSprite
@@ -33,6 +34,12 @@ public class Tower extends AnimatedSprite
 //		PhysicsFactory.createCircleBody(this.mPhysicsWorld, this, BodyType.StaticBody, fixtureDef, PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT);
 		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(this, mBody, true, true));
 	
+	}
+	
+	@Override
+	public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY)
+	{
+		return true;
 	}
 	
 
